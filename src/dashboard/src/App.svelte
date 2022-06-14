7933dbea9d71c58527be80b3fc92d13ad03c3c90
Vallasc <giacomovallorani.97@gmail.com>
2022-06-14 13:07:20 +0200

    Add navbar

--- src/dashboard/src/App.svelte
+++ src/dashboard/src/App.svelte
@@ -1,19 +1,96 @@
 <script lang="ts">
-  //import logo from './assets/svelte.png'
-  import Signin from './lib/Signin.svelte'
-  import Dashboard from './lib/Dashboard.svelte'
+  import { Router, Route, links, navigate } from "svelte-navigator"
+  import Nav from './lib/components/Nav.svelte'
+  import NavItem from './lib/components/NavItem.svelte'
+  import { jwtToken } from "./stores"
+  import Signin from "./lib/routes/Signin.svelte"
+  import Signup from "./lib/routes/Signup.svelte"
+  import Page404 from "./lib/routes/404.svelte"
+  import Content from "./lib/routes/Content.svelte"
+  import Profile from "./lib/routes/Profile.svelte"
+  import Devices from "./lib/routes/Devices.svelte"
+  import UpdateRoute from "./lib/components/UpdateRoute.svelte"
+  import Toast from "./lib/components/Toast.svelte"
+  import { init, signOut } from "./lib/logic"
+
+  init()
 </script>
 
-<Dashboard></Dashboard>
+<div class="main" use:links>
+  {#if $jwtToken == null}
+    <Router>
+      <Route path="/signin">
+        <Signin/>
+      </Route>
+      <Route path="/signup">
+          <Signup/>
+      </Route>
+      <Route>
+				{navigate("/signin")}
+			</Route>
+    </Router>
+  {:else}
+    <Nav>
+      <h2 slot="title" class="title">LOMO</h2>
+      <NavItem slot="item-t1" link="/devices">Devices</NavItem>
+      <NavItem slot="item-t2" indent={1} link="/devices/giardino1">Giardino 1</NavItem>
+
+      <NavItem slot="item-t3" indent={1} link="/devices/giardino1/dashboard">- Dashboard</NavItem>
+      <NavItem slot="item-t4" indent={1} link="/devices/giardino1/settings">- Settings</NavItem>
+      <NavItem slot="item-t5" indent={1} link="/devices/giardino1/map">- Map</NavItem>
+      <NavItem slot="item-b1" link="/telegram">Telegram</NavItem>
+      <NavItem slot="item-b2" link="/profile">Profile</NavItem>
+      <NavItem slot="item-b3" link="/signout">Sign out</NavItem>
+      <Router slot="content">
+        <UpdateRoute/>
+        <Route path="/">
+          {navigate("/devices")}
+        </Route>
+        <Route path="/devices">
+          <Devices/>
+        </Route>
+        <Route path="/profile">
+          <Profile/>
+        </Route>
+        <Route path="/signout">
+          {signOut()}
+          {navigate("/")}
+        </Route>
+        <Route>
+          <Page404></Page404>
+        </Route>
+      </Router>
+    </Nav>
+  {/if}
+</div>
+
+<Toast/>
 
 <style>
-    /*:global(p.dark-mode) {
-        color: white;
-    }*/
-    :global(html, body, #app) {
-      height: 100%;
-    }
-    :global(.nav-item) {
-      margin-right: 0px;
-    }
+  /*:global(p.dark-mode) {
+      color: white;
+  }*/
+  :global(html, body, #app) {
+    height: 100%;
+  }
+  :global(.nav-item) {
+    margin-right: 0px;
+  }
+
+  :global(h1:focus, h2:focus, h3:focus) {
+    outline-color: transparent;
+  }
+
+  .main {
+		display: flex;
+		flex-direction: column;
+		align-items: stretch;
+		background-color: #ffffff;
+		height: 100%;
+		width: 100%;
+	}
+
+  .title {
+    margin-bottom: 0px;
+  }
 </style>
