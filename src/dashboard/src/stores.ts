9febf54b08e2c171d5149bf90f1f4d482195b4ce
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-06-29 18:41:34 +0200

    Add selectedDevice

--- src/dashboard/src/stores.ts
+++ src/dashboard/src/stores.ts
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store'
 
-export const jwtToken = writable("null")
+export const jwtToken = writable(null)
 export const location = writable("/")
+export const selectedDevice = writable(null)
 
 export const toast = writable({
     title: "",
