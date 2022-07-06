44107c04565612dab6e7a28b7cf03becbd09021c
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-07-06 15:45:51 +0200

    Add sensors.h

--- src/lomo_device/include/config.h
+++ src/lomo_device/include/config.h
@@ -1,8 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define DHTPIN 23
-#define DHTTYPE DHT11   // DHT 11
+#define DHT_PIN 27
+#define DHT_TYPE DHT11
+
+#define GAS_PIN 4
+#define SOIL_PIN 26
+#define BUTTON_PIN 22
 
 
 #endif
