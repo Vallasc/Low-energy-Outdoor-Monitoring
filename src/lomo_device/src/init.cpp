75dbfd17f108f7c2e14f76f65cb13bf49b63ca78
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-07-07 16:07:03 +0200

    Code refactor

--- src/lomo_device/src/init.cpp
+++ src/lomo_device/src/init.cpp
@@ -2,17 +2,8 @@
 
 void InitAPWebServer::init()
 {
-  // Set static IP
-  IPAddress AP_LOCAL_IP(192, 168, 1, 10);
-  IPAddress AP_GATEWAY_IP(192, 168, 1, 254);
-  IPAddress AP_NETWORK_MASK(255, 255, 255, 0);
-  if (!WiFi.softAPConfig(AP_LOCAL_IP, AP_GATEWAY_IP, AP_NETWORK_MASK)) {
-    Serial.println("AP Config Failed");
-    return;
-  }
-  IPAddress IP = WiFi.softAPIP();
-  Serial.println("Static IP: " + AP_LOCAL_IP.toString());
-
+  
+  wifi_create_AP();
   // Setup web server connection
   this->server = new WebServer(80);
   this->server->enableCORS(true);
@@ -42,56 +33,56 @@ void InitAPWebServer::handle_post_init()
     deserializeJson(doc, plain);
     JsonObject obj = doc.as<JsonObject>();
 
-    open_preferences();
+    Preferences* pref = open_preferences();
     if(obj.containsKey(JSON_ID)){
       String id = obj[JSON_ID];
-      preferences.putString(DEVICE_ID, id);
+      pref->putString(KEY_DEVICE_ID, id);
     }
     if(obj.containsKey(JSON_NAME)){
       String name = obj[JSON_NAME];
-      preferences.putString(DEVICE_NAME, name);
+      pref->putString(KEY_DEVICE_NAME, name);
     }
     if(obj.containsKey(JSON_PROTOCOL)){
       String protocol = obj[JSON_PROTOCOL];
-      preferences.putString(PROTOCOL_TYPE, protocol);
+      pref->putString(KEY_PROTOCOL_TYPE, protocol);
     }
     if(obj.containsKey(JSON_SAMPLE_FREQUENCY)){
       int sampleFrequency = obj[JSON_SAMPLE_FREQUENCY];
-      preferences.putInt(SAMPLE_FREQUENCY, sampleFrequency);
+      pref->putInt(KEY_SAMPLE_FREQUENCY, sampleFrequency);
     }
     if(obj.containsKey(JSON_MIN_GAS_VALUE)){
       int minGasValue = obj[JSON_MIN_GAS_VALUE];
-      preferences.putInt(MIN_GAS_VALUE, minGasValue);
+      pref->putInt(KEY_MIN_GAS_VALUE, minGasValue);
     }
     if(obj.containsKey(JSON_MAX_GAS_VALUE)){
       int maxGasValue = obj[JSON_MAX_GAS_VALUE];
-      preferences.putInt(MAX_GAS_VALUE, maxGasValue);
+      pref->putInt(KEY_MAX_GAS_VALUE, maxGasValue);
     }
     if(obj.containsKey(JSON_PROXY_PORT)){
       int proxyPort = obj[JSON_PROXY_PORT];
-      preferences.putInt(PROXY_PORT, proxyPort);
+      pref->putInt(KEY_PROXY_PORT, proxyPort);
     }
     if(obj.containsKey(JSON_MQTT_PORT)){
       int mqttPort = obj[JSON_MQTT_PORT];
-      preferences.putInt(MQTT_PORT, mqttPort);
+      pref->putInt(KEY_MQTT_PORT, mqttPort);
     }
     if(obj.containsKey(JSON_HOST)){
       String host = obj[JSON_HOST];
-      preferences.putString(HOST, host);
+      pref->putString(KEY_HOST, host);
     }
     if(obj.containsKey(JSON_TOKEN)){
       String host = obj[JSON_TOKEN];
-      preferences.putString(TOKEN, host);
+      pref->putString(KEY_TOKEN, host);
     }
     if(obj.containsKey(JSON_WIFI_SSID)){
       String wfiSsid = obj[JSON_WIFI_SSID];
-      preferences.putString(WIFI_SSID, wfiSsid);
+      pref->putString(KEY_WIFI_SSID, wfiSsid);
     }
     if(obj.containsKey(JSON_WIFI_PASS)){
       String wifiPassword = obj[JSON_WIFI_PASS];
-      preferences.putString(WIFI_PASS, wifiPassword);
+      pref->putString(KEY_WIFI_PASS, wifiPassword);
     }
-    preferences.putBool(DEVICE_CONFIGURED, true);
+    preferences.putBool(KEY_DEVICE_CONFIGURED, true);
     close_preferences();
   }
 
