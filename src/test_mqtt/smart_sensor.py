e4ef58b4a973696851786d596ff2f3f26dcd5b40
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-07-07 16:03:29 +0200

    Update

--- src/test_mqtt/smart_sensor.py
+++ src/test_mqtt/smart_sensor.py
@@ -1,25 +1,44 @@
-"""
-MQTT Smart temperature Sensor
-"""
-
 import time
 
 import paho.mqtt.client as mqtt
 from faker import Faker
 
-# let's connect to the MQTT broker
-MQTT_BROKER_URL    = "mqtt.eclipseprojects.io"
-MQTT_PUBLISH_TOPIC = "temperature"
+MQTT_BROKER_URL    = "localhost"
+
+TEMP_TOPIC = "devices/foo/temp"
+SOIL_TOPIC = "devices/foo/soil"
+HUM_TOPIC = "devices/foo/hum"
+AQI_TOPIC = "devices/foo/aqi"
+
+MQTT_USER = "admin"
+MQTT_PASS = "admin"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("devices/#")
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
 
 mqttc = mqtt.Client()
+mqttc.username_pw_set(MQTT_USER, MQTT_PASS)
 mqttc.connect(MQTT_BROKER_URL)
 
-# Init faker our fake data provider sostituire con i dati veri
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
 fake = Faker()
 
-# Infinite loop of fake data sent to the Broker
 while True:
-    temperature = fake.random_int(min=0, max=30)
-    mqttc.publish(MQTT_PUBLISH_TOPIC, temperature)
-    print(f"Published new temperature measurement: {temperature}")
+    temp = fake.random_int(min=10, max=30)
+    soil = fake.random_int(min=0, max=255)
+    hum = fake.random_int(min=0, max=100)
+    aqi = fake.random_int(min=0, max=10)
+
+    mqttc.loop()
+    mqttc.publish(TEMP_TOPIC, str(temp))
+    mqttc.publish(SOIL_TOPIC, str(soil))
+    mqttc.publish(HUM_TOPIC, str(hum))
+    mqttc.publish(AQI_TOPIC, str(aqi))
+
     time.sleep(1)
