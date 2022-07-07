760042ec323e4184d018eee7eae0589eb32d7eab
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-07-08 00:15:10 +0200

    Change String with const char*

--- src/lomo_device/src/mqtt_manager.cpp
+++ src/lomo_device/src/mqtt_manager.cpp
@@ -1,53 +1,76 @@
 #include "mqtt_manager.h"
 
-bool MQTTManager::connect()
+bool MQTTManager::begin()
 {
-    int i = this->max_mqtt_retries;
-    while (!client->connected() && i-- > 0) {
-        Serial.print("Connecting to MQTT broker ");
-        if (client->connect(device_id, device_id, token)) {
-            Serial.println("connected");
-        } else {
-            Serial.print("failed, rc=");
-            Serial.print(client->state());
-            delay(5000);
-        }
+  int i = this->max_mqtt_retries;
+  while (!client->connected() && i-- > 0)
+  {
+    Serial.print("Connecting to MQTT broker ");
+    if (client->connect(device_id, device_id, token))
+    {
+      Serial.println("connected");
     }
-    if(!client->connected() ){
-        Serial.print("Broker not connected: max mqtt retries reached.");
-        return false;
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client->state());
+      delay(5000);
     }
-    return true;
+  }
+  if (!client->connected())
+  {
+    Serial.print("Broker not connected: max mqtt retries reached.");
+    return false;
+  }
+  return true;
 }
 
 void MQTTManager::publish_temperature(float value)
 {
-    publish(TEMP, String(value));
+  char buff[8];
+  snprintf(buff, sizeof(buff), "%.2f", value);
+  publish(TEMP, buff);
 }
 
 void MQTTManager::publish_humidity(float value)
 {
-    publish(HUM, String(value));
+  char buff[8];
+  snprintf(buff, sizeof(buff), "%.2f", value);
+  publish(HUM, buff);
 }
 
 void MQTTManager::publish_aqi(float value)
 {
-    publish(AQI, String(value));
+  char buff[8];
+  snprintf(buff, sizeof(buff), "%.2f", value);
+  publish(AQI, buff);
 }
 
 void MQTTManager::publish_soil(float value)
 {
-    publish(SOIL, String(value));
+  char buff[8];
+  snprintf(buff, sizeof(buff), "%.2f", value);
+  publish(SOIL, buff);
 }
 
-void MQTTManager::publish(String topic, String value) 
+void MQTTManager::publish(const char* topic, const char* value)
 {
-    String full_topic = String(MAIN_MQTT_TOPIC) + "/" + String(device_id) + "/" + topic;
-    Serial.println(full_topic);
-    client->publish((char * ) full_topic.c_str(), (char * ) value.c_str());
+  char full_topic[256];
+  snprintf(full_topic, sizeof(full_topic), "%s/%s/%s", MAIN_MQTT_TOPIC, device_id, topic);
+  Serial.println(full_topic);
+  client->publish(full_topic, value);
+}
+
+void MQTTManager::publish_sensors(float temp, float hum, float soil, float aqi)
+{
+  Serial.println("Publish sensors MQTT");
+  publish_temperature(temp);
+  publish_humidity(hum);
+  publish_soil(soil);
+  publish_aqi(aqi);
 }
 
 bool MQTTManager::is_connected()
 {
-    return client->connected();
+  return client->connected();
 }
