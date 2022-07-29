4bbeb9a9a1eadf0e9988511d439c4a3ddf6779fb
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-07-29 23:31:28 +0200

    Add user

--- src/web_interface/src/stores.ts
+++ src/web_interface/src/stores.ts
@@ -2,6 +2,7 @@ import { writable } from 'svelte/store'
 
 export const jwtToken = writable(null)
 export const location = writable("/")
+export const user = writable(null)
 export const selectedDevice = writable(null)
 
 export const toast = writable({
