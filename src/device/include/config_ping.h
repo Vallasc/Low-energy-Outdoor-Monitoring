fd986afa3714da76ef545ab1569c6506ecfe6e24
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-07-29 23:24:10 +0200

    Add update_config with metrics

--- src/device/include/config_ping.h
+++ src/device/include/config_ping.h
@@ -9,12 +9,13 @@
 class ConfigPing
 {
   public:
-    ConfigPing(WiFiUDP* _udp_client, const char* _host, int _remote_port, const char* _id, const char* _token, InitServer* _init)
+    ConfigPing(WiFiUDP* _udp_client, const char* _host, int _remote_port, const char* _id, const char* _token, const char* _ssid, InitServer* _init)
     {
       udp_client = _udp_client;
-      strcpy(id, _id);
-      strcpy(token, _token);
-      strcpy(host, _host);
+      strncpy(id, _id, sizeof(id));
+      strncpy(token, _token, sizeof(token));
+      strncpy(host, _host,sizeof(host));
+      strncpy(ssid, _ssid, sizeof(ssid));
       remote_port= _remote_port;
       init = _init;
     }
@@ -24,14 +25,50 @@ class ConfigPing
       udp_client->begin(LOCAL_PORT);
     }
 
+    bool update_config(
+      unsigned long total_mqtt_packet_count, 
+      unsigned long received_mqtt_packet_count,
+      unsigned long total_http_packet_count,
+      unsigned long received_http_packet_count,
+      unsigned long mqtt_mean_time,
+      unsigned long http_mean_times)
+    {
+      // Send ping
+      udp_client->beginPacket(host, remote_port);
+      udp_client->printf("{\"id\": \"%s\", "
+                          "\"token\": \"%s\", "
+                          "\"host\": \"%s\", "
+                          "\"wifiSsid\": \"%s\", "
+                          "\"totalMqttPacketCount\": %lu, "
+                          "\"receivedMqttPacketCount\": %lu, "
+                          "\"totalHttpPacketCount\": %lu, "
+                          "\"receivedHttpPacketCount\": %lu, "
+                          "\"mqttMeanTime\": %lu, "
+                          "\"httpMeanTime\": %lu }", 
+                          id, token, host, ssid,
+                          total_mqtt_packet_count, received_mqtt_packet_count, 
+                          total_http_packet_count, received_http_packet_count, 
+                          mqtt_mean_time, http_mean_times);
+      udp_client->endPacket();
+      Serial.println("Ping done");
+      return read_response();
+    }
+
     bool update_config()
     {
       // Send ping
       udp_client->beginPacket(host, remote_port);
-      udp_client->printf("{\"id\": \"%s\", \"token\": \"%s\"}", id, token);
+      udp_client->printf("{\"id\": \"%s\", "
+                          "\"token\": \"%s\", "
+                          "\"host\": \"%s\", "
+                          "\"wifiSsid\": \"%s\"}", id, token, host, ssid);
       udp_client->endPacket();
       Serial.println("Ping done");
+      return read_response();
+    }
 
+    bool read_response()
+    {
       bool readPacket = false;
       int retries = 10;
       while (!readPacket && retries-- > 0)
@@ -62,9 +99,10 @@ class ConfigPing
   private:
     WiFiUDP *udp_client;
     char id[32];
-    char token[32];
+    char token[64];
     char packet[255];
     char host[32];
+    char ssid[32];
     int remote_port;
     InitServer* init;
 };
