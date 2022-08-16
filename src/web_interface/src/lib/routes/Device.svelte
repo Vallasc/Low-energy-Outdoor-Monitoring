3317675944ba5e13bae694832c0e41c6f5b2b02c
Vallasc <giacomovallorani.97@gmail.com>
2022-08-16 16:09:41 +0200

    Update dashboard url

--- src/web_interface/src/lib/routes/Device.svelte
+++ src/web_interface/src/lib/routes/Device.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-  import { selectedDevice, user } from "../../stores";
+  import { selectedDevice, user, location } from "../../stores";
   import { fade } from 'svelte/transition'
   import { getUser } from '../logic'
 
   export let deviceId = ''
-
-  // let url = "#"
-  // src="http://192.168.1.229:3333{$selectedDevice.dashboardUrl}?orgId=1&refresh=5s&theme=light&kiosk"
-  // TODO sistema iframe che carica prima di avere l'id della dashboard
+  let url = window.location.protocol + "//" + window.location.hostname
 
   async function init() {
     console.log("init")
@@ -16,6 +13,9 @@
     for(let d of $user.devices)
       if(d.id == deviceId){
         $selectedDevice = d
+        let iframe: any = document.getElementById("dashboard-iframe")
+        iframe.src = url + ":3333" + $selectedDevice.dashboardUrl + "?orgId=1&refresh=5s&theme=light&kiosk"
+        console.log(iframe.src)
         return
       }
   }
@@ -27,7 +27,10 @@
 <div class="mb-3 mt-3" in:fade = {{duration: 200}}>
   <h3>Grafana dashboard</h3>
 </div>
-<iframe in:fade = {{delay: 1000, duration: 200}} title="Grafana dashboard" src="http://192.168.1.229:3333{$selectedDevice.dashboardUrl}?orgId=1&refresh=5s&theme=light&kiosk">
+<iframe 
+    id="dashboard-iframe" in:fade = {{delay: 1000, duration: 200}} 
+    title="Grafana dashboard"
+    src="{url}:3333{$selectedDevice.dashboardUrl}?orgId=1&refresh=5s&theme=light&kiosk">
 </iframe>
 
 <style>
