54e1b7b3ca1db342c7dce8c21e9e9b4852e26ccf
Vallasc <vallasc@protonmail.com>
2022-09-08 17:35:33 +0000

    Minor changes

--- src/api_server/src/deviceConfig.js
+++ src/api_server/src/deviceConfig.js
@@ -1,6 +1,6 @@
 export const MIN_GAS_VALUE = process.env.MIN_GAS_VALUE || 10
 export const MAX_GAS_VALUE = process.env.MAX_GAS_VALUE || 70
-export const SAMPLE_FREQUENCY = process.env.SAMPLE_FREQUENCY || 1
+export const SAMPLE_FREQUENCY = process.env.SAMPLE_FREQUENCY || 2
 export const CONFIG_UPDATE_FREQUENCY = process.env.CONFIG_UPDATE_FREQUENCY || 1
 export const PROTOCOL = process.env.PROTOCOL || "MQTT"
 export const PROXY_PORT = process.env.PROXY_PORT || 8082
