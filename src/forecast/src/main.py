e2ac9ef49c6b4dca299fbc6f5bc6d339e3aca9a2
Vallasc <vallasc@protonmail.com>
2022-09-08 11:24:03 +0000

    Final release

--- src/forecast/src/main.py
+++ src/forecast/src/main.py
@@ -2,13 +2,12 @@ import os
 import pandas as pd
 from pandas.tseries.offsets import DateOffset
 import time
-# import matplotlib.pyplot as plt
 import logging
 import datetime 
 from prophet import Prophet
 from influxdb_client import InfluxDBClient, Point, WriteOptions
-from influxdb_client.client.write_api import SYNCHRONOUS
-# from sklearn.metrics import mean_squared_error
+from influxdb_client.client.write_api import WriteType
+from mongo_client import MongoClient
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -21,81 +20,69 @@ INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "lomo")
 INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET", "lomo")
 INFLUXDB_TOKEN = os.getenv("INFLUX_TOKEN", "adminadminadmin")
 
-TIME_DATASET = 60 * 15
-TIME_FORECASTING = 60 * 5
-DEVICE_ID = '6313235cdbeac3dbeabbf087'
+SLEEP_TIME = int(os.getenv("SLEEP_TIME", "10"))
+FIELDS = [ 'temp', 'gas', 'hum', 'soil']
 
-now = datetime.datetime.now()
-START_TIME = (now - datetime.timedelta(seconds=TIME_DATASET)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-STOP_TIME = now.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+def result_to_dataframe(result):
+    raw = []
+    for table in result:
+        for record in table.records:
+            raw.append((record.get_value(), record.get_time()))
+    return pd.DataFrame(raw, columns=['y','ds'], index=None)
 
+if __name__ == '__main__':
+    mongo_client = MongoClient()
 
-def success_cb(details, data):
-    url, token, org = details
-    print(url, token, org)
-    data = data.decode('utf-8').split('\n')
-    print('Total Rows Inserted:', len(data))  
+    client = InfluxDBClient(url="http://"+INFLUXDB_HOST+":"+INFLUXDB_PORT, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 
-def error_cb(details, data, exception):
-    print(exc)
+    while True:
+        users = mongo_client.get_users()
+        for user in users:
+            for device in user['devices']:
+                if device['enablePrediction']:
+                    logging.info('Processing device ' + str(device['id']))
+                    for field in FIELDS:
+                        logging.info('Field ' + field)
+                        try:
+                            now = datetime.datetime.now()
+                            START_TIME = (now - datetime.timedelta(minutes=device['trainingTime'])).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+                            STOP_TIME = now.strftime("%Y-%m-%dT%H:%M:%S.%fZ") 
+                            query = 'from(bucket: "lomo")' \
+                                ' |> range(start:' +  START_TIME + ', stop:' + STOP_TIME + ')'\
+                                ' |> filter(fn: (r) => r["_measurement"] == "devices")' \
+                                ' |> filter(fn: (r) => r["_field"] == "' + field + '")' \
+                                ' |> filter(fn: (r) => r["id"] == "' + str(device['id']) +'")'
+                                    
+                            result = client.query_api().query(org=INFLUXDB_ORG, query=query)
 
-def retry_cb(details, data, exception):
-    print('Retrying because of an exception:', exc)    
+                            # #Convert the results to dataframe
+                            df = result_to_dataframe(result)
+                            df['ds'] = df['ds'].dt.tz_localize(None)
+                            # logging.info(df.head())
 
-client = InfluxDBClient(url="http://"+INFLUXDB_HOST+":"+INFLUXDB_PORT, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
-query_api = client.query_api()
-write_api = client.write_api(write_options=SYNCHRONOUS, success_callback=success_cb, error_callback=error_cb)
+                            # Fit the model by instantiating a new Prophet object and passing in the historical DataFrame
+                            m = Prophet(changepoint_prior_scale=0.01).fit(df)
 
+                            # Use the helper method Prophet.make_future_dataframe to prepare your dataframe for forecasting
+                            # periods specifies the number of time series points you'd like to forecast onto 
+                            # freq time between points 
+                            per = int(device['predictionTime'])
+                            future = m.make_future_dataframe(periods=per, freq= DateOffset(minutes=1))
+                            forecast = m.predict(future)
+                            forecast['ds'] = forecast.ds.dt.floor('min')
+                            # logging.info(forecast.head())
+                            # Convert the DataFrame to Line Protocol:
+                            cp = forecast[['ds', 'yhat']].copy()
+                            lines = ['devices,id=' + str(device['id']) + ' forecasted_' + field + '=' + str(cp["yhat"][d])
+                                    + ' ' + str(int(time.mktime(cp['ds'][d].timetuple()))) + "000000000" for d in range(len(cp))]
+                            write_client = client.write_api(
+                                                write_options=WriteOptions(batch_size=1000, flush_interval=10_000,
+                                                            jitter_interval=2_000, retry_interval=5_000, write_type=WriteType.synchronous))
+                            write_client.write(INFLUXDB_BUCKET, INFLUXDB_ORG, lines)
+                            write_client.__del__()
 
-query = 'from(bucket: "lomo")' \
-  ' |> range(start:' +  START_TIME + ', stop:' + STOP_TIME + ')'\
-  ' |> filter(fn: (r) => r["_measurement"] == "devices")' \
-  ' |> filter(fn: (r) => r["_field"] == "temp")' \
-  ' |> filter(fn: (r) => r["id"] == "' + DEVICE_ID +'")'
-        
-result = client.query_api().query(org=INFLUXDB_ORG, query=query)
-
-# #Convert the results into a list:
-raw = []
-for table in result:
-    for record in table.records:
-        raw.append((record.get_value(), record.get_time()))
-# logging.info(raw[0:5])
-
-
-#Convert raw data to DataFrame
-logging.info("influxdb query into dataframe")
-df = pd.DataFrame(raw, columns=['y','ds'], index=None)
-df['ds'] = df['ds'].dt.tz_localize(None)
-logging.info(df.head())
-
-# #Fit the model by instantiating a new Prophet object and passing in the historical DataFrame
-m = Prophet(changepoint_prior_scale=0.01).fit(df)
-
-# Use the helper method Prophet.make_future_dataframe to prepare your dataframe for forecasting
-# periods specifies the number of time series points you'd like to forecast onto 
-# freq time between points 
-
-per = int(TIME_FORECASTING / 60)
-future = m.make_future_dataframe(periods=per, freq= DateOffset(minutes=1))
-forecast = m.predict(future)
-forecast['ds'] = forecast.ds.dt.floor('min')
-logging.info(forecast.head())
-
-# #Convert the DataFrame to Line Protocol:
-
-cp = forecast[['ds', 'yhat']].copy()
-
-# weather,location=us-midwest temperature=82 1465839830100400200
-lines = ['devices,id=' + DEVICE_ID + ' forcasted_temp=' + str(cp["yhat"][d])
-         + ' ' + str(int(time.mktime(cp['ds'][d].timetuple()))) + "000000000" for d in range(len(cp))]
-
-print(lines)
-_write_client = client.write_api(
-                  write_options=WriteOptions(batch_size=1000, flush_interval=10_000,
-                                              jitter_interval=2_000, retry_interval=5_000))
-_write_client.write(INFLUXDB_BUCKET, INFLUXDB_ORG, lines)
-_write_client.__del__()
-
-# # mse = mean_squared_error(df['y'], forecast['yhat'])
-# # print('MSE: %f' % mse)
+                            # mse = mean_squared_error(df['y'], forecast['yhat'])
+                            # print('MSE: %f' % mse)
+                        except Exception:
+                            logging.error("Error processing " + str(device['id']))
+        time.sleep(SLEEP_TIME)
