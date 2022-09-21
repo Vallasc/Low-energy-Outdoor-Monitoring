4a75915c4fdac5f59c6aa068eb7f7c8e7473c314
Vallasc <vallasc@protonmail.com>
2022-09-21 10:35:03 +0000

    Prophet tweaks

--- src/forecast/src/main.py
+++ src/forecast/src/main.py
@@ -37,7 +37,7 @@ def result_to_dataframe(result):
 
 
 def calc_forecasting(device, field, start_time, stop_time):
-    query = 'from(bucket: "lomo")' \
+    query = 'from(bucket: "' + INFLUXDB_BUCKET + '")' \
         ' |> range(start:' +  start_time + ', stop:' + stop_time + ')'\
         ' |> filter(fn: (r) => r["_measurement"] == "devices")' \
         ' |> filter(fn: (r) => r["_field"] == "' + field + '")' \
@@ -48,7 +48,16 @@ def calc_forecasting(device, field, start_time, stop_time):
     df = result_to_dataframe(result)
     df['ds'] = df['ds'].dt.tz_localize(None)
     # Fit the model by instantiating a new Prophet object and passing in the historical DataFrame
-    m = Prophet(changepoint_prior_scale=0.01).fit(df)
+    m = Prophet(
+        yearly_seasonality=False,
+        weekly_seasonality=False,
+        daily_seasonality=20, # 20
+        n_changepoints=50, # 30
+        changepoint_range=0.8, # 0.8
+        changepoint_prior_scale=0.5, # 0.5
+        # interval_width=1.0
+    )
+    m.fit(df)
     # periods specifies the number of time series points you'd like to forecast onto 
     # freq time between points 
     per = int(device['predictionTime'])
@@ -63,40 +72,6 @@ def calc_forecasting(device, field, start_time, stop_time):
     write_client.write(INFLUXDB_BUCKET, INFLUXDB_ORG, lines)
     write_client.__del__()
 
-def calc_mse(device, field, start_time, stop_time):
-    query = 'from(bucket: "lomo")' \
-        ' |> range(start:' +  start_time + ', stop:' + stop_time + ')'\
-        ' |> filter(fn: (r) => r["_measurement"] == "devices")' \
-        ' |> filter(fn: (r) => r["_field"] == "' + field + '")' \
-        ' |> filter(fn: (r) => r["id"] == "' + str(device['id']) +'")'
-    result = client.query_api().query(org=INFLUXDB_ORG, query=query)
-    # Convert the results to dataframe
-    df = result_to_dataframe(result)
-    df['ds'] = df['ds'].dt.tz_localize(None)
-    train, test = train_test_split(df, test_size=0.2, shuffle=False)
-    m = Prophet(changepoint_prior_scale=0.01).fit(train)
-    per = int(test.size * device['sampleFrequency'])
-    future = m.make_future_dataframe(periods=per, freq= DateOffset(minutes=1))
-    forecast = m.predict(future)
-
-    forecast['ds'] = forecast.ds.dt.floor('min')
-    test['ds'] = test.ds.dt.floor('min')
-    metric = test.set_index('ds')[['y']].join(forecast.set_index('ds').yhat).reset_index()
-    mse = mean_squared_error(metric['y'], metric['yhat'])
-    r2 = r2_score(metric['y'], metric['yhat'])
-    logging.info(mse)
-
-    lines = ['devices,id=' + str(device['id']) + ' forecasted_analysis_' + field + '=' + str(forecast["yhat"][d])
-                                    + ' ' + str(int(time.mktime(forecast['ds'][d].timetuple()))) + "000000000" for d in range(len(forecast))]
-    write_client = client.write_api(write_options=WriteOptions(batch_size=1000, flush_interval=10_000,
-                                                            jitter_interval=2_000, retry_interval=5_000, write_type=WriteType.synchronous))
-    write_client.write(INFLUXDB_BUCKET, INFLUXDB_ORG, lines)
-    p = Point("devices").tag("id", str(device['id'])).field("mse_" + field, float(mse))
-    write_client.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=p)
-    p = Point("devices").tag("id", str(device['id'])).field("r2_" + field, float(r2))
-    write_client.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=p)
-    write_client.__del__()
-
 if __name__ == '__main__':
     while True:
         users = mongo_client.get_users()
@@ -106,16 +81,14 @@ if __name__ == '__main__':
                     logging.info('Processing device ' + str(device['id']))
                     for field in FIELDS:
                         logging.info('Field ' + field)
-                        try:
-                            now = datetime.datetime.now()
-                            START_TIME = '-5y'
-                            if device['trainingTime'] > 0:
-                                START_TIME = (now - datetime.timedelta(minutes=device['trainingTime'])).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-                            STOP_TIME = now.strftime("%Y-%m-%dT%H:%M:%S.%fZ") 
-                            
-                            calc_forecasting(device, field, START_TIME, STOP_TIME)
-                            # Calc MSE
-                            # calc_mse(device, field, START_TIME, STOP_TIME)
-                        except Exception:
-                             logging.error("Error processing " + str(device['id']))
+                        # try:
+                        now = datetime.datetime.now()
+                        START_TIME = '-5y'
+                        if device['trainingTime'] > 0:
+                            START_TIME = (now - datetime.timedelta(minutes=device['trainingTime'])).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+                        STOP_TIME = now.strftime("%Y-%m-%dT%H:%M:%S.%fZ") 
+                        
+                        calc_forecasting(device, field, START_TIME, STOP_TIME)
+                        # except Exception:
+                        #      logging.error("Error processing " + str(device['id']))
         time.sleep(SLEEP_TIME)
